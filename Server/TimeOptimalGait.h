7d02a78db36c384475ebce87f9ec7566cc18e9a9
hex <hex@hex.com>
2017-09-26 21:22:31 +0800

    PI devided by 900, iteration seems to be right, but still has a problem: switch point on ds_upBound_vLmt cannot be known

--- Server/TimeOptimalGait.h
+++ Server/TimeOptimalGait.h
@@ -68,10 +68,10 @@ private:
 //    const int stanceLegID[3] {1,3,5};
 
 
-    double s_b[1501] {0};
-    double b_sb[1501] {0};
-    double db_sb[1501] {0};
-    double ddb_sb[1501] {0};
+    double s_b[2251] {0};
+    double b_sb[2251] {0};
+    double db_sb[2251] {0};
+    double ddb_sb[2251] {0};
 
     double Jvi[9] {0};
     double abs_param_dds[18] {0};//for vLmt of stanceLeg
@@ -81,104 +81,104 @@ private:
     double param_a0L[18] {0};
     double param_a0H[18] {0};
 
-    double output_PeeB[1501][18] {{0}};
-    double output_dsds[1501][18] {{0}};
-    double output_ds[1501][18] {{0}};
-    double output_const[1501][18] {{0}};
-    double output_const1[1501][18] {{0}};
-    double output_const2[1501][18] {{0}};
-    double output_dds[1501][18] {{0}};
-    double output_a2[1501][18] {{0}};
-    double output_a1[1501][18] {{0}};
-    double output_a0L[1501][18] {{0}};
-    double output_a0H[1501][18] {{0}};
+    double output_PeeB[2251][18] {{0}};
+    double output_dsds[2251][18] {{0}};
+    double output_ds[2251][18] {{0}};
+    double output_const[2251][18] {{0}};
+    double output_const1[2251][18] {{0}};
+    double output_const2[2251][18] {{0}};
+    double output_dds[2251][18] {{0}};
+    double output_a2[2251][18] {{0}};
+    double output_a1[2251][18] {{0}};
+    double output_a0L[2251][18] {{0}};
+    double output_a0H[2251][18] {{0}};
 
-    double ds_upBound_aLmt_body[1501] {0};
-    double ds_upBound_vLmt_body[1501] {0};
-    double dds_upBound_body[1501] {0};
-    double dds_lowBound_body[1501] {0};
-    double ds_upBound_body[1501] {0};
+    double ds_upBound_aLmt_body[2251] {0};
+    double ds_upBound_vLmt_body[2251] {0};
+    double dds_upBound_body[2251] {0};
+    double dds_lowBound_body[2251] {0};
+    double ds_upBound_body[2251] {0};
 
-//    double slopedsBound_body[1501] {0};
-//    double paramdds0Point_body[1501][18] {{0}};
-//    double tangentPoint_body[1501] {0};
+//    double slopedsBound_body[2251] {0};
+//    double paramdds0Point_body[2251][18] {{0}};
+//    double tangentPoint_body[2251] {0};
 //    int paramdds0Count_body[18] {0};
 //    int tangentCount_body {0};
 
-    double slopeDelta_body[1501] {0};
-    int isParamddsExact0_body[1501][18] {{0}};
-    double switchPoint_body[1501] {0};
-    int switchScrewID_body[1501] {0};
+    double slopeDelta_body[2251] {0};
+    int isParamddsExact0_body[2251][18] {{0}};
+    double switchPoint_body[2251] {0};
+    int switchScrewID_body[2251] {0};
     int switchCount_body {0};
 
-    double real_ds_body[1501] {0};
-    double real_dds_body[1501] {0};
-    double ds_forward_body[1501] {0};
-    double ds_backward_body[1501] {0};
-    double dds_forward_body[1501] {0};
-    double dds_backward_body[1501] {0};
+    double real_ds_body[2251] {0};
+    double real_dds_body[2251] {0};
+    double ds_forward_body[2251] {0};
+    double ds_backward_body[2251] {0};
+    double dds_forward_body[2251] {0};
+    double dds_backward_body[2251] {0};
 
-    double timeArray_body[1501] {0};
+    double timeArray_body[2251] {0};
     double Tstep {1};
     double s_b1 {0};
     double s_b2 {0};
 
-//    double pb_sw[1501] {0};
-//    double vb_sw[1501] {0};
-//    double ab_sw[1501] {0};
-//    double pb_sw_tmp[1501] {0};
-//    double vb_sw_tmp[1501] {0};
-//    double ab_sw_tmp[1501] {0};
-    double output_init_pva_b[1501][3] {{0}};
-    double pva_b[1501][3] {{0}};
+//    double pb_sw[2251] {0};
+//    double vb_sw[2251] {0};
+//    double ab_sw[2251] {0};
+//    double pb_sw_tmp[2251] {0};
+//    double vb_sw_tmp[2251] {0};
+//    double ab_sw_tmp[2251] {0};
+    double output_init_pva_b[2251][3] {{0}};
+    double pva_b[2251][3] {{0}};
 
     int swCount {0};
-    double s_w[601][6] {{0}};
+    double s_w[901][6] {{0}};
     double f_sw[3] {0};
     double df_sw[3] {0};
     double ddf_sw[3] {0};
 
-    double ds_upBound_aLmt[601][6] {{0}};
-    double ds_lowBound_aLmt[601][6] {{0}};
-    double ds_upBound_vLmt[601][6] {{0}};
-    double ds_upBound[601][6] {{0}};
-    double ds_lowBound[601][6] {{0}};
-    double dds_lowBound[601][6] {{0}};
-    double dds_upBound[601][6] {{0}};
+    double ds_upBound_aLmt[901][6] {{0}};
+    double ds_lowBound_aLmt[901][6] {{0}};
+    double ds_upBound_vLmt[901][6] {{0}};
+    double ds_upBound[901][6] {{0}};
+    double ds_lowBound[901][6] {{0}};
+    double dds_lowBound[901][6] {{0}};
+    double dds_upBound[901][6] {{0}};
 
-//    double slopedsBound[601][6] {{0}};
-//    double paramdds0Point[601][18] {{0}};
-//    double tangentPoint[601][6] {{0}};
+//    double slopedsBound[901][6] {{0}};
+//    double paramdds0Point[901][18] {{0}};
+//    double tangentPoint[901][6] {{0}};
 //    int paramdds0Count[18] {0};
 //    int tangentCount[6] {0};
 
-    double slopeDelta[601][6] {{0}};
-    int isParamddsExact0[601][18] {{0}};
-    double switchPoint[601][6] {{0}};
-    int switchScrewID[601][6] {{0}};
+    double slopeDelta[901][6] {{0}};
+    int isParamddsExact0[901][18] {{0}};
+    double switchPoint[901][6] {{0}};
+    int switchScrewID[901][6] {{0}};
     int switchCount[6] {0};
 
-    double ds_forward[601][6] {{0}};
-    double ds_backward[601][6] {{0}};
-    double dds_forward[601][6] {{0}};
-    double dds_backward[601][6] {{0}};
-    double real_ds[601][6] {{0}};
-    double real_dds[601][6] {{0}};
-//    double real_ddsMax[601][6] {{0}};
-//    double real_ddsMin[601][6] {{0}};
-    double real_ds_tmp[601][6] {{0}};
+    double ds_forward[901][6] {{0}};
+    double ds_backward[901][6] {{0}};
+    double dds_forward[901][6] {{0}};
+    double dds_backward[901][6] {{0}};
+    double real_ds[901][6] {{0}};
+    double real_dds[901][6] {{0}};
+//    double real_ddsMax[901][6] {{0}};
+//    double real_ddsMin[901][6] {{0}};
+    double real_ds_tmp[901][6] {{0}};
 
-    double timeArray[601][6] {{0}};
-    double timeArray_tmp[601][6] {{0}};
+    double timeArray[901][6] {{0}};
+    double timeArray_tmp[901][6] {{0}};
     double maxTime {0};
     int maxTotalCount {0};
 //    int maxTotalCount_last {1};
 //    int maxTimeID {0};
 
-//    double output_Pee[601][18] {{0}};
-//    double output_Pin[601][18] {{0}};
-//    double output_Vin[601][18] {{0}};
-//    double output_Ain[601][18] {{0}};
+//    double output_Pee[901][18] {{0}};
+//    double output_Pin[901][18] {{0}};
+//    double output_Vin[901][18] {{0}};
+//    double output_Ain[901][18] {{0}};
 };
 
 #endif
