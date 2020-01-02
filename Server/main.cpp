6dc4c533b5ff7e0a36289a65e8a1e9ddd5befc2d
czj-laptop <czj2097@gmail.com>
2020-01-01 20:18:12 -0800

    make problem exists, solving, sync

--- Server/main.cpp
+++ Server/main.cpp
@@ -19,95 +19,12 @@ using namespace std;
 #include "unistd.h"
 #endif
 
-#include "Move_Gait.h"
-#include "GeneralFunc.h"
-#include "NormalGait.h"
-
-//#include "Balance.h"
-#include "OpenDoor.h"
-
 #include "NonRTOptimalBCS.h"
 #include "NonRTOptimalGCS.h"
-#include "RTOptimal.h"
 
 
 int main(int argc, char *argv[])
 {
-    /*
-    //test IMU//
-    double peIMU2Body[6] {0};//{0,0,0,0,-PI/2,PI};
-    double pmIMU2Body[4][4] {0};
-    double pmBody2IMU[4][4] {0};
-    pmIMU2Body[0][0] = -1;
-    pmIMU2Body[1][2] = -1;
-    pmIMU2Body[2][1] = -1;
-    pmIMU2Body[3][3] = 1;
-    aris::dynamic::s_pm2pe(*pmIMU2Body,peIMU2Body,"313");
-    aris::dynamic::s_inv_pm(*pmIMU2Body,*pmBody2IMU);
-    printf("peIMU2Body:%.4f,%.4f,%.4f,%.4f,%.4f,%.4f\n",peIMU2Body[0],peIMU2Body[1],peIMU2Body[2],peIMU2Body[3],peIMU2Body[4],peIMU2Body[5]);
-
-    double peIMU2IMUGround[6]{0,0,0,PI,0,PI};
-    double pmIMU2IMUGround[4][4];
-    aris::dynamic::s_pe2pm(peIMU2IMUGround,*pmIMU2IMUGround,"321");
-
-    double peIMUGround2BodyGround[6] {0};//{0,0,0,0,-PI/2,PI};
-    double pmIMUGround2BodyGround[4][4] {0};
-    pmIMUGround2BodyGround[0][0] = 1;
-    pmIMUGround2BodyGround[1][2] = 1;
-    pmIMUGround2BodyGround[2][1] = -1;
-    pmIMUGround2BodyGround[3][3] = 1;
-    aris::dynamic::s_pm2pe(*pmIMUGround2BodyGround,peIMUGround2BodyGround);
-    printf("peIMUGround2BodyGround:%.4f,%.4f,%.4f,%.4f,%.4f,%.4f\n",peIMUGround2BodyGround[0],peIMUGround2BodyGround[1],peIMUGround2BodyGround[2],peIMUGround2BodyGround[3],peIMUGround2BodyGround[4],peIMUGround2BodyGround[5]);
-
-    double pm[4][4] {0};
-    aris::dynamic::s_pm_dot_pm(*pmIMUGround2BodyGround,*pmIMU2IMUGround,*pmBody2IMU,*pm);
-    printf("pmBody2BodyGround:\n");
-    for (int i=0;i<4;i++)
-    {
-        for(int j=0;j<4;j++)
-        {
-            printf("%.4f,",pm[i][j]);
-        }
-        printf("\n");
-    }
-    printf("\n");
-
-    printf("pmImuGround2BodyGround:\n");
-    for (int i=0;i<4;i++)
-    {
-        for(int j=0;j<4;j++)
-        {
-            printf("%.4f,",pmIMUGround2BodyGround[i][j]);
-        }
-        printf("\n");
-    }
-    printf("\n");
-
-    printf("pmImu2ImuGround:\n");
-    for (int i=0;i<4;i++)
-    {
-        for(int j=0;j<4;j++)
-        {
-            printf("%.4f,",pmIMU2IMUGround[i][j]);
-        }
-        printf("\n");
-    }
-    printf("\n");
-
-    printf("pmBodu2Imu:\n");
-    for (int i=0;i<4;i++)
-    {
-        for(int j=0;j<4;j++)
-        {
-            printf("%.4f,",pmBody2IMU[i][j]);
-        }
-        printf("\n");
-    }
-    printf("\n");
-    */
-
-//    FastWalk::TimeOptimalGait1by1();
-//    FastWalk::WalkPYAnalyse();
 //    TimeOptimalGait optimalgait;
 //    optimalgait.GetOptimalDsByMajorIteration();
 //    optimalgait.GetOptimalGait2s();
@@ -115,11 +32,6 @@ int main(int argc, char *argv[])
 //    optimalgait.OutputData();
 //    optimalgait.GetNormalGait();
 //    optimalgait.GetEntireGait();
-
-    //FastWalk::JointSpaceWalk jointspacewalker;
-    //FastWalk::FastWalkPY pyfastwalker;
-    //ForceTask::ForceWalk forcewalker;
-    //NormalGait::StartRecordData();
 
     std::string xml_address;
 
@@ -151,16 +63,6 @@ int main(int argc, char *argv[])
     rs.addCmd("rc", Robots::recoverParse, Robots::recoverGait);
     rs.addCmd("wk", Robots::walkParse, Robots::walkGait);
     rs.addCmd("ro", Robots::resetOriginParse, Robots::resetOriginGait);
-
-    rs.addCmd("mwr",NormalGait::parseMoveWithRotate,NormalGait::moveWithRotate);
-
-    rs.addCmd("cmb",ForceTask::parseContinueMoveBegin,ForceTask::continueMove);
-    rs.addCmd("cmj",ForceTask::parseContinueMoveJudge,ForceTask::continueMove);
-    rs.addCmd("ffd",Robots::walkParse, ForceTask::forceForward);
-
-    //rs.addCmd("jfw",jointspacewalker.parseJointSpaceFastWalk,jointspacewalker.jointSpaceFastWalk);
-    //rs.addCmd("fsw",pyfastwalker.parseFastWalkByPY,pyfastwalker.fastWalkByPY);
-    //rs.addCmd("fcw",forcewalker.parseForceWalk,forcewalker.forceWalk);
 
     rs.open();
 
